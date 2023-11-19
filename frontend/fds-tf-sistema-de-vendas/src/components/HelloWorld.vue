16c2733f2d8163e68d60a3d2cf882aa6517ce204
Lorran Marques <70297511+LGMarques9963@users.noreply.github.com>
2023-11-19 01:10:28 -0300

    Axios

--- frontend/fds-tf-sistema-de-vendas/src/components/HelloWorld.vue
+++ frontend/fds-tf-sistema-de-vendas/src/components/HelloWorld.vue
@@ -1,73 +1,19 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-title>Clientes</v-card-title>
+          <v-card-text>
+            <v-list>
+              <v-list-item v-for="cliente in clientes" :key="cliente.id">
+                <v-list-item-content>
+                  <v-list-item-title>{{ cliente.nome }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -128,6 +74,23 @@ export default {
         href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
       },
     ],
+    clientes: [],
   }),
+  methods: {
+    getClientes() {
+      this.$http
+        .get("/clientes")
+        .then((response) => {
+          this.clientes = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+
+  mounted() {
+    this.getClientes();
+  },
 };
 </script>
