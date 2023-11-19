a3c7ca73dd4ffe2be30ff4ab948df326edd95ecc
Lorran Marques <70297511+LGMarques9963@users.noreply.github.com>
2023-11-19 16:18:52 -0300

    NavigationDrawer

---
+++ frontend/fds-tf-sistema-de-vendas/src/components/AppBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar app color="secondary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/LGMarques9963/FDS-TF-Sistema-de-Vendas"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">GitHub</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/fds-tf-sistema-de-vendas/src/components/NavigationDrawer.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-card>
+        <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
+            <v-list-item class="px-2">
+                <v-list-item-avatar>
+                    <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-title>John Leider</v-list-item-title>
+
+                <v-btn icon @click.stop="mini = !mini">
+                    <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "NavigationDrawer",
+
+    data: () => ({
+        drawer: true,
+        mini: false,
+        items: [
+            { title: "Clientes", icon: "mdi-inbox" },
+            { title: "Produtos", icon: "mdi-inbox-arrow-up" },
+            { title: "Pedidos", icon: "mdi-heart" },
+            { title: "Orcamentos", icon: "mdi-archive" },
+            { title: "Estatisticas", icon: "mdi-trash-can" },
+        ],
+    }),
+}
+</script>
+
+<style></style>
