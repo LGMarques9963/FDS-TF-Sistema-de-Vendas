cef8f837201b697f9cba3a26d6756acb7588026b
Lorran Marques <70297511+LGMarques9963@users.noreply.github.com>
2023-11-20 00:05:19 -0300

    Estatisticas

--- frontend/fds-tf-sistema-de-vendas/src/components/AppBar.vue
+++ frontend/fds-tf-sistema-de-vendas/src/components/AppBar.vue
@@ -1,25 +1,11 @@
 <template>
   <v-app-bar app color="secondary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2>
+          TF Sistemas de Vendas
+        </h2>
       </div>
-
       <v-spacer></v-spacer>
 
       <v-btn
---
+++ frontend/fds-tf-sistema-de-vendas/src/views/EstatisticasView.vue
@@ -0,0 +1,18 @@
+<template>
+    <estatisticas />
+</template>
+
+<script>
+import Estatisticas from '../components/Estatisticas.vue';
+export default {
+    name: "EstatisticasView",
+    components: {
+        Estatisticas,
+    },
+
+}
+</script>
+
+<style>
+
+</style>
